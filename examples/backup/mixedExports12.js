c0a92f460d4e4de985c48a90c36bd07cf0f6b0db
Andrey <joseph.ramirez@growsari.com>
2021-12-07 06:07:46 +0800

    Add files for anon single param with mixed exports

---
+++ examples/backup/mixedExports12.js
@@ -0,0 +1,8 @@
+export default params => {
+  console.log(params);
+};
+
+export const foo = 'foo';
+export const bar = 'bar';
+export const fizz = 'fizz\n';
+export const buzz = 'buzz';
---
+++ examples/src/mixedExports13.js
@@ -0,0 +1,7 @@
+export default params => {
+  console.log(params);
+};
+
+export { default as HelloWorld } from '../HelloWorld';
+export { default as FooBar } from '../../FooBar';
+export { default as FizzBuzz } from '../../../FizzBuzz';
