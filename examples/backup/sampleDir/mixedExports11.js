06d712a1e99da2c31c63c2a0c4e3dce79bd1c861
Andrey <joseph.ramirez@growsari.com>
2021-12-07 05:38:23 +0800

    Add files for existing export default with named exports or aggregating

---
+++ examples/backup/sampleDir/mixedExports11.js
@@ -0,0 +1,7 @@
+import SingleFoo from './SingleFoo';
+
+export { default as HelloWorld } from '../HelloWorld';
+export { default as FooBar } from '../../FooBar';
+export { default as FizzBuzz } from '../../../FizzBuzz';
+
+export default SingleFoo;
