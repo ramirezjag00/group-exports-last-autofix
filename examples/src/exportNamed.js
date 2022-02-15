5da4d516789c038e4d7884511fe603c1167ecfe7
Andrey <joseph.ramirez@growsari.com>
2022-02-15 14:45:23 +0800

    Change exportNamed asset

--- examples/src/exportNamed.js
+++ examples/src/exportNamed.js
@@ -1,7 +1,9 @@
 import SampleFile from './SampleFile';
 // comment here
 
-export const foo = 'foo';
-export const bar = 'bar';
-export const fizz = 'fizz\n';
-export const buzz = 'buzz';
+const foo = 'foo';
+const bar = 'bar';
+const fizz = 'fizz\n';
+const buzz = 'buzz';
+
+export { foo, bar, fizz, buzz, };
